7d2dc74098d481786d182cfa84fe95250119d929
KDHen <money6845@naver.com>
2023-11-29 16:27:50 +0900

    .

--- src/hf/dp/IteratorPattern/MenuTestDrive.java
+++ src/hf/dp/IteratorPattern/MenuTestDrive.java
@@ -1,7 +1,11 @@
 package hf.dp.IteratorPattern;
 
+import hf.dp.IteratorPattern.MyHWInfo;
+
 public class MenuTestDrive {
     public static void main(String[] args) {
+        MyHWInfo.printInfo();
+
         PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
         DinerMenu dinerMenu = new DinerMenu();
         Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
